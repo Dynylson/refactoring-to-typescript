6f34d3c2249f4a57218ab467bbf68d6c69809e08
Dynylson <dynylsonjr@gmail.com>
2022-06-16 15:22:15 -0300

    Tipagem components/ModalEditFood terminada

--- src/components/ModalEditFood/index.tsx
+++ src/components/ModalEditFood/index.tsx
@@ -6,43 +6,55 @@ import Modal from "../Modal";
 import Input from "../Input";
 import { FormHandles } from "@unform/core";
 
-interface ModalAddFoodProps {
+interface ModalEditFoodProps {
   isOpen: boolean;
   setIsOpen: () => void;
-  handleAddFood: (data: AddFood) => void;
+  handleUpdateFood: (data: EditFood) => void;
+  editingFood: IFood;
 }
 
-interface AddFood {
+interface IFood {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  available: boolean;
+  image: string;
+}
+
+interface EditFood {
   image: string;
   name: string;
   price: string;
   description: string;
 }
 
-const ModalAddFood = ({
+const ModalEditFood = ({
   isOpen,
   setIsOpen,
-  handleAddFood,
-}: ModalAddFoodProps) => {
+  handleUpdateFood,
+  editingFood,
+}: ModalEditFoodProps) => {
   const formRef = useRef<FormHandles>(null);
 
-  const handleSubmit = async (data: AddFood) => {
-    handleAddFood(data);
+  const handleSubmit = async (data: EditFood) => {
+    handleUpdateFood(data);
     setIsOpen();
   };
 
   return (
     <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
-      <Form ref={formRef} onSubmit={handleSubmit}>
-        <h1>Novo Prato</h1>
+      <Form ref={formRef} onSubmit={handleSubmit} initialData={editingFood}>
+        <h1>Editar Prato</h1>
         <Input name="image" placeholder="Cole o link aqui" />
 
         <Input name="name" placeholder="Ex: Moda Italiana" />
         <Input name="price" placeholder="Ex: 19.90" />
 
         <Input name="description" placeholder="Descrição" />
-        <button type="submit" data-testid="add-food-button">
-          <p className="text">Adicionar Prato</p>
+
+        <button type="submit" data-testid="edit-food-button">
+          <div className="text">Editar Prato</div>
           <div className="icon">
             <FiCheckSquare size={24} />
           </div>
@@ -52,4 +64,4 @@ const ModalAddFood = ({
   );
 };
 
-export default ModalAddFood;
+export default ModalEditFood;
